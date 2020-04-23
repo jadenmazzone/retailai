38160e5c919da20e37e36963a2bb50ec73372023
Jaden <jaden1999@hotmail.ca>
2020-04-22 17:47:19 -0700

    Updates

---
+++ precache-manifest.60fa56f739e6ca6e80caaf999c68f4d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d7e6942246c27b27373f2f1f5699de",
+    "url": "/retailai/index.html"
+  },
+  {
+    "revision": "95be05433aed22708366",
+    "url": "/retailai/static/css/main.fc5535f1.chunk.css"
+  },
+  {
+    "revision": "6512f1a1ad7b81178dc5",
+    "url": "/retailai/static/js/2.dd95909e.chunk.js"
+  },
+  {
+    "revision": "1cee6415d6a23ee9df20d6d1b452ff0b",
+    "url": "/retailai/static/js/2.dd95909e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95be05433aed22708366",
+    "url": "/retailai/static/js/main.4f725fb0.chunk.js"
+  },
+  {
+    "revision": "88557fdb71cc6d7312b8",
+    "url": "/retailai/static/js/runtime-main.17188902.js"
+  },
+  {
+    "revision": "f94c54d803aabd0d10e3d735d29b4818",
+    "url": "/retailai/static/media/campaign-revenue.f94c54d8.png"
+  },
+  {
+    "revision": "b8e2baf6b3c212de1b1c0c8e112f28e6",
+    "url": "/retailai/static/media/pictureOfMe.b8e2baf6.jpg"
+  }
+]);
